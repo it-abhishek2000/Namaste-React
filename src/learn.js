95e0a155095f60f6ff6f1ad92d59533e64ac666f
itabhishek2000 <it.abhishek2000@gmail.com>
2022-11-25 14:05:53 +0530

    day 3 Homework

--- src/learn.js
+++ src/learn.js
@@ -1,5 +1,33 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
-const heading = React.createElement("h1",{},"Hello Abhisheeek");
+import ReactDOM from "react-dom/client";
+const heading = React.createElement("h1", {}, "Hello Abhisheeek");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+//root.render(heading);
+const divWithChild = React.createElement("div",{id:"title"}, [
+  React.createElement("h1", {id:"heading1"}, "Hello Abhisheeek"),
+  React.createElement("h1", {id:"heading2"}, "Hello Abhisheeek"),
+  React.createElement("h1", {id:"heading3"}, "Hello Abhisheeek")
+]);
+
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>This is my FirstComponent {5 + 6} </h1>
+      <SecondComponent />
+      <SecondComponent />
+      <SecondComponent />
+       {divWithChild}
+    </div>
+  );
+};
+const SecondComponent = () => {
+  return (
+    <div>
+      <h1>This is my FirstComponent {5 + 6} </h1>
+    </div>
+  );
+};
+//root.render(divWithChild)
+root.render(<FirstComponent />);
