3e44a90d1a830f79b2202ab46e034b7d63421fd4
itabhishek2000 <it.abhishek2000@gmail.com>
2022-12-10 23:06:27 +0530

    day5 Homework

--- session4/app.js
+++ session4/app.js
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom/client";
-import { Data } from "./data.js";
+import data from "./data.json";
 import Card from "./cardcomponent.js";
+import SeachBar from "./searchbar.js";
+import { useState } from "react";
+ 
+const  root = ReactDom.createRoot(document.getElementById("root"));
 
-root = ReactDom.createRoot(document.getElementById("root"));
-
+const Title = () => (
+  <h1 className="teamtitle">Hiii, We are Briany Fools</h1>
+);
 const HeadingWithSeachBar = () => {
   return (
     <div className="header">
-      <h1 className="teamtitle">Hiii, We are Briany Fools</h1>
-      <input className="search" type="search" placeholder="search"></input>
+      <Title/>
+      <SeachBar/>
     </div>
   );
 };
 
-const AllCard = () =>
-  Data.map((x) => {
-    return <Card member={x} key={x.id} />;
+const AllCard = ({searchResult}) =>
+  searchResult.map((x) => {
+    return <Card member={x} key = {x.id}/>;
   });
 
 const Cardconatiner = () => {
+  const [searchResult,setSearchResult] = useState(data);
   return (
-    <>
-      <HeadingWithSeachBar />
-      <div className="Allcard">
-        <AllCard />
+    <> <Title/>
+      <SeachBar setSearchResult={setSearchResult}/>
+      <div className="Allcard" >
+        <AllCard searchResult={searchResult}/>
       </div>
     </>
   );
 };
 
-root.render(<Cardconatiner />);
+root.render(<Cardconatiner/>);
